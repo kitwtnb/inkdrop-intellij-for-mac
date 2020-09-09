ef5f47dc38019b60b9fa232aec959e6a5d7956e2
Keita Watanabe <watanabe.kit@gmail.com>
2020-09-09 19:10:32 +0900

    add test for toggle comment

--- test/its.js
+++ test/its.js
@@ -1,58 +1,79 @@
 const expect = chai.expect;
 
 function its(command, params) {
-    params.forEach(p => {
-        it(`value "${p.initValue}" should be converted to "${p.expectValue}"`, () => {
-            // setup
-            const cm = CodeMirror.fromTextArea(cm_text);
-            cm.setValue(p.initValue);
-            cm.setSelection(p.initSelection.from, p.initSelection.to);
-            const cmc = new CodeMirrorClient(cm);
+  params.forEach((p) => {
+    context(
+      `value: "${p.initValue.replace("\n", "'\\n'")}", from: "${JSON.stringify(
+        p.initSelection.from
+      )}", to: "${JSON.stringify(p.initSelection.to)}"`,
+      () => {
+        it(`value "${p.initValue.replace(
+          "\n",
+          "'\\n'"
+        )}" should be converted to "${p.expectValue.replace(
+          "\n",
+          "'\\n'"
+        )}"`, () => {
+          // setup
+          const cm = CodeMirror.fromTextArea(cm_text);
+          cm.setValue(p.initValue);
+          cm.setSelection(p.initSelection.from, p.initSelection.to);
+          const cmc = new CodeMirrorClient(cm);
 
-            // exercise
-            command(cmc);
+          // exercise
+          command(cmc);
 
-            // verify
-            expect(p.expectValue).equal(cm.getValue(), '`value` was wrong');
-
-            // teardown
-            cm.toTextArea();
+          // verify
+          cm.toTextArea();
+          expect(p.expectValue).equal(cm.getValue(), "`value` was wrong");
         });
 
-        it(`from "${JSON.stringify(p.initSelection.from)}" should be converted to "${JSON.stringify(p.expectSelection.from)}"`, () => {
-            // setup
-            const cm = CodeMirror.fromTextArea(cm_text);
-            cm.setValue(p.initValue);
-            cm.setSelection(p.initSelection.from, p.initSelection.to);
-            const cmc = new CodeMirrorClient(cm);
+        it(`from "${JSON.stringify(
+          p.initSelection.from
+        )}" should be converted to "${JSON.stringify(
+          p.expectSelection.from
+        )}"`, () => {
+          // setup
+          const cm = CodeMirror.fromTextArea(cm_text);
+          cm.setValue(p.initValue);
+          cm.setSelection(p.initSelection.from, p.initSelection.to);
+          const cmc = new CodeMirrorClient(cm);
 
-            // exercise
-            command(cmc);
+          // exercise
+          command(cmc);
 
-            // verify
-            const area = cmc.selectedArea();
-            expect(p.expectSelection.from).to.deep.equal(area.from, "`from` of the cursor was wrong");
-
-            // teardown
-            cm.toTextArea();
+          // verify
+          const area = cmc.selectedArea();
+          cm.toTextArea();
+          expect(p.expectSelection.from).to.deep.equal(
+            area.from,
+            "`from` of the cursor was wrong"
+          );
         });
 
-        it(`to "${JSON.stringify(p.initSelection.to)}" should be converted to "${JSON.stringify(p.expectSelection.to)}"`, () => {
-            // setup
-            const cm = CodeMirror.fromTextArea(cm_text);
-            cm.setValue(p.initValue);
-            cm.setSelection(p.initSelection.from, p.initSelection.to);
-            const cmc = new CodeMirrorClient(cm);
+        it(`to "${JSON.stringify(
+          p.initSelection.to
+        )}" should be converted to "${JSON.stringify(
+          p.expectSelection.to
+        )}"`, () => {
+          // setup
+          const cm = CodeMirror.fromTextArea(cm_text);
+          cm.setValue(p.initValue);
+          cm.setSelection(p.initSelection.from, p.initSelection.to);
+          const cmc = new CodeMirrorClient(cm);
 
-            // exercise
-            command(cmc);
+          // exercise
+          command(cmc);
 
-            // verify
-            const area = cmc.selectedArea();
-            expect(p.expectSelection.to).to.deep.equal(area.to, "`to` of the cursor was wrong");
-
-            // teardown
-            cm.toTextArea();
+          // verify
+          const area = cmc.selectedArea();
+          cm.toTextArea();
+          expect(p.expectSelection.to).to.deep.equal(
+            area.to,
+            "`to` of the cursor was wrong"
+          );
         });
-    });
+      }
+    );
+  });
 }
--- test/test-param.js
+++ test/test-param.js
@@ -1,22 +1,22 @@
 function param(initValue, initSelection, expectValue, expectSelection) {
-    return {
-        initValue: initValue,
-        initSelection: initSelection,
-        expectValue: expectValue,
-        expectSelection: expectSelection
-    };
+  return {
+    initValue: initValue,
+    initSelection: initSelection,
+    expectValue: expectValue,
+    expectSelection: expectSelection,
+  };
 }
 
 function selection(from, to) {
-    return {
-        from: from,
-        to: to
-    };
+  return {
+    from: from,
+    to: to,
+  };
 }
 
 function cursor(line, ch) {
-    return {
-        line: line,
-        ch: ch
-    };
+  return {
+    line: line,
+    ch: ch,
+  };
 }
