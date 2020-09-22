19e33de20fd12e14731945477fc1fbd77073ca94
Keita Watanabe <watanabe.kit@gmail.com>
2020-09-22 17:05:49 +0900

    run prettier

--- test/lib/command/duplicate-current-line-test.js
+++ test/lib/command/duplicate-current-line-test.js
@@ -6,19 +6,37 @@ describe("duplicate-current-line", () => {
       const initValue = "";
       const initSelection = selection(cursor(0, 0));
 
-      its(command, [
-        param(initValue, initSelection, initValue, initSelection)
-      ]);
+      its(command, [param(initValue, initSelection, initValue, initSelection)]);
     });
 
     context("value is a character", () => {
       const initValue = "a";
 
       its(command, [
-        param(initValue, selection(cursor(0, 0)), "a\na", selection(cursor(1, 0))),
-        param(initValue, selection(cursor(0, 1)), "a\na", selection(cursor(1, 1))),
-        param(initValue, selection(cursor(0, 0), cursor(0, 1)), "aa", selection(cursor(0, 1), cursor(0, 2))),
-        param(initValue, selection(cursor(0, 1), cursor(0, 0)), "aa", selection(cursor(0, 1), cursor(0, 2))),
+        param(
+          initValue,
+          selection(cursor(0, 0)),
+          "a\na",
+          selection(cursor(1, 0))
+        ),
+        param(
+          initValue,
+          selection(cursor(0, 1)),
+          "a\na",
+          selection(cursor(1, 1))
+        ),
+        param(
+          initValue,
+          selection(cursor(0, 0), cursor(0, 1)),
+          "aa",
+          selection(cursor(0, 1), cursor(0, 2))
+        ),
+        param(
+          initValue,
+          selection(cursor(0, 1), cursor(0, 0)),
+          "aa",
+          selection(cursor(0, 1), cursor(0, 2))
+        ),
       ]);
     });
 
@@ -26,11 +44,36 @@ describe("duplicate-current-line", () => {
       const initValue = "abc\ndef";
 
       its(command, [
-        param(initValue, selection(cursor(0, 0)), "abc\nabc\ndef", selection(cursor(1, 0))),
-        param(initValue, selection(cursor(0, 3)), "abc\nabc\ndef", selection(cursor(1, 3))),
-        param(initValue, selection(cursor(0, 0), cursor(0, 1)), "aabc\ndef", selection(cursor(0, 1), cursor(0, 2))),
-        param(initValue, selection(cursor(0, 0), cursor(1, 0)), "aabc\nabc\ndef", selection(cursor(1, 0), cursor(2, 0))),
-        param(initValue, selection(cursor(0, 2), cursor(1, 1)), "abc\ndc\ndef", selection(cursor(1, 1), cursor(2, 1)))
+        param(
+          initValue,
+          selection(cursor(0, 0)),
+          "abc\nabc\ndef",
+          selection(cursor(1, 0))
+        ),
+        param(
+          initValue,
+          selection(cursor(0, 3)),
+          "abc\nabc\ndef",
+          selection(cursor(1, 3))
+        ),
+        param(
+          initValue,
+          selection(cursor(0, 0), cursor(0, 1)),
+          "aabc\ndef",
+          selection(cursor(0, 1), cursor(0, 2))
+        ),
+        param(
+          initValue,
+          selection(cursor(0, 0), cursor(1, 0)),
+          "aabc\nabc\ndef",
+          selection(cursor(1, 0), cursor(2, 0))
+        ),
+        param(
+          initValue,
+          selection(cursor(0, 2), cursor(1, 1)),
+          "abc\ndc\ndef",
+          selection(cursor(1, 1), cursor(2, 1))
+        ),
       ]);
     });
   });
